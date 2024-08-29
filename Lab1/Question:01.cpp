00df8239f4e4c9b3e7330a9f020c078d9a1ee270
Umaima <umaimarauf2005@gmail.com>
2024-08-29 17:00:53 +0500

    Update Question:01.cpp

--- Lab1/Question:01.cpp
+++ Lab1/Question:01.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main(){
-    int row,col;
-    int arr[row][col];
-    cout<<"Enter number of rows :";
-    cin>>row;
-    cout<<"Enter number of columns :";
-    cin>>col;
-    cout<<"Enter array data :"<<endl;
-    for (int i=0;i<row;i++){
-        for (int j=0;j<col;j++){
-            cout<<"Enter data :";
-            cin>>arr[i][j];
+int main() {
+    int row, col;
+
+    cout << "Enter number of rows: ";
+    cin >> row;
+    cout << "Enter number of columns: ";
+    cin >> col;
+
+    int arr[row][col]; 
+    cout << "Enter array data:" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << "Enter data for position (" << i << "," << j << "): ";
+            cin >> arr[i][j];
         }
     }
-    int ans[row*col];
-    int k=0;
-    for (int i=0;i<row;i++){
-        for (int j=0;j<col;j++){
-            ans[k]=arr[j][i];
+    int ans[row * col];  
+    int k = 0;
+    for (int j = 0; j < col; j++) {  
+        for (int i = 0; i < row; i++) {
+            ans[k] = arr[i][j];
             k++;
         }
     }
-    cout<<"Column major order of the matrix is :"<<endl;
-    for (int i=0;i<k;i++){
-        cout<<i<<"  ";
+    cout << "Column major order of the matrix is: " << endl;
+    for (int i = 0; i < k; i++) {
+        cout << ans[i] << "  ";  
     }
+    return 0;
 }
