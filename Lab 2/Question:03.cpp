faa61837444b24b81d4fbe7b32ac9d1b77438578
Umaima <umaimarauf2005@gmail.com>
2024-09-12 19:43:10 +0500

    Update Question:03.cpp

--- Lab 2/Question:03.cpp
+++ Lab 2/Question:03.cpp
@@ -2,16 +2,19 @@
 #include <vector>
 using namespace std;
 
-void inputdata(vector<int>& A, vector<int>& B) {
-    cout << "Enter elements for array1:" << endl;
-    for (int i = 0; i < A.size(); ++i) {
-        cin >> A[i];
+class Umaima_Lab02 {
+public:
+    void inputdata(vector<int>& A, vector<int>& B) {
+        cout << "Enter elements for array1:" << endl;
+        for (int i = 0; i < A.size(); ++i) {
+            cin >> A[i];
+        }
+        cout << "Enter elements for array2:" << endl;
+        for (int i = 0; i < B.size(); ++i) {
+            cin >> B[i];
+        }
     }
-    cout << "Enter elements for array2:" << endl;
-    for (int i = 0; i < B.size(); ++i) {
-        cin >> B[i];
-    }
-}
+};
 int main() {
     int no1, no2;
     cout << "Enter the number of elements for array1: ";
@@ -22,14 +25,13 @@ int main() {
     vector<int> A(no1);
     vector<int> B(no2);
 
-    inputdata(A, B);
-
+    Umaima_Lab02 lab;
+    lab.inputdata(A, B);
     int X, Y;
     cout << "Enter the scalar value X: ";
     cin >> X;
     cout << "Enter the scalar value Y: ";
     cin >> Y;
-
     if (A.size() != B.size()) {
         cout << "Arrays must have the same number of elements for operations." << endl;
         return 1;
@@ -40,18 +42,18 @@ int main() {
     for (int i = 0; i < size; ++i) {
         result[i] = X * A[i] + Y * B[i];
     }
-
     cout << "Result of XA + YB: (";
     for (int i = 0; i < size; ++i) {
-        cout << result[i] << ",";
+        cout << result[i];
+        if (i != size - 1) {
+            cout << ",";
+        }
     }
-    cout << ")"<<endl;
-
+    cout << ")" << endl;
     int dotProduct = 0;
     for (int i = 0; i < size; ++i) {
         dotProduct += A[i] * B[i];
     }
-
     cout << "Result of A . B: " << dotProduct << endl;
     return 0;
 }
